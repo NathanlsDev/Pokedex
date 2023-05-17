0149f32b83acd3fcd5775c5e345f54463d43dda0
Nathan Lima <nathanls.dev@gmail.com>
2023-05-16 23:46:07 -0300

    func encapsulation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,34 @@
 const pokemonsList = document.querySelector("#orderedPokemonsLi");
+const loadMoreButton = document.querySelector("#loadMoreButton");
+const limit = 6;
+let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-  return `
-    <li class="pokemon ${pokemon.type}">
-      <span class="number">#${pokemon.number}</span>
-      <span class="name">${pokemon.name} </span>
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon) => `
+      <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
 
-      <div class="detail">
-        <ol class="types">
-          ${pokemon.types
-            .map((type) => `<li class="type ${type}">${type}</li>`)
-            .join("")}
-        </ol>
+        <div class="detail">
+          <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+          </ol>
 
-        <img src="${pokemon.photo}" alt="${pokemon.name}">
-      </div>
-    </li>
-  `;
+          <img src="${pokemon.photo}"
+                alt="${pokemon.name}">
+        </div>
+      </li>
+    
+    `).join('')
+    pokemonsList.innerHTML += newHtml
+  })
 }
 
-pokeApi.getPokemons().then((pokemons = []) => {
-  const newHtml = (pokemonsList.innerHTML = pokemons
-    .map(convertPokemonToLi)
-    .join(""));
-  pokemonsList.innerHTML = newHtml;
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit
+  loadPokemonItens(offset, limit);
+
 });
